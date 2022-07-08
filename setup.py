7f611874b92f46d84985205b3cef56144a922e03
yuyecreus <creus99@gmail.com>
2022-07-08 11:26:06 +0200

    ready to pypi

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup 
+from pathlib import Path
 
-setup(name='wave_defense', version='0.0.1', install_requires=['gym', 'pygame', 'numpy'], author = "Roger Creus", description="A Reinforcement Learning environment inspired from wave defense games")
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='wave_defense', version='0.0.1', install_requires=['gym', 'pygame', 'numpy'], author = "Roger Creus", long_description=long_description, long_description_content_type='text/markdown')
