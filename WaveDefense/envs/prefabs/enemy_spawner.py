83e3b8a61c31d7127d5a2855da833826169e683f
roger-creus <creus99@gmail.com>
2022-09-07 10:41:31 -0400

    fixed enemies spawning too far from screen[A

--- WaveDefense/envs/prefabs/enemy_spawner.py
+++ WaveDefense/envs/prefabs/enemy_spawner.py
@@ -14,17 +14,17 @@ class EnemySpawner():
     def spawn_enemy(self):
         if random.randint(0,1) == 0:
             if random.randint(0,1):
-                pos_x = random.randint(self.width, self.width + 100)
+                pos_x = self.width + 5
                 pos_y = random.randint(0, self.height)           
             else:
-                pos_x = random.randint(-100, -10)
+                pos_x = -5
                 pos_y = random.randint(0, self.height)
         else:
             if random.randint(0,1):
-                pos_y = random.randint(self.height, self.height + 100)
+                pos_y = self.height + 5
                 pos_x = random.randint(0, self.width)
             else:
-                pos_y = random.randint(-100, -10)
+                pos_y = -5
                 pos_x = random.randint(0, self.width)
 
         normal_enemy = NormalEnemy(self.player, pos_x, pos_y)
