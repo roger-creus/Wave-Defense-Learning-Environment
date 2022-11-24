2e159d30e4558fcc4abfb76c15b8fcea9b293ffb
roger-creus <creus99@gmail.com>
2022-11-24 11:25:14 -0500

    ready to publish

--- WaveDefense/random_agent.py
+++ WaveDefense/random_agent.py
@@ -1,23 +1,14 @@
 import gym
 import WaveDefense
-from envs.wave_defense_tabular import WaveDefenseTabular
-from envs.wave_defense import WaveDefense
-import matplotlib.pyplot as plt
-from IPython import embed
 
-#env = WaveDefenseTabular(seed = 200)
 env = gym.make("WaveDefense-v1")
 env.seed(50)
 
 obs = env.reset()
 done = False
-steps = 0
-
 
 while not done:
-    steps+=1
     action = env.action_space.sample()
     obs, reward, done, info = env.step(action)
     env.render()
 
-print(steps)
--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='WaveDefense',
-    version='1.3', 
-    install_requires=['gym', 'pygame', 'numpy'], 
+    version='1.4', 
+    install_requires=['gym==0.17.0', 'pygame', 'numpy'], 
     py_modules=['WaveDefense', 'WaveDefense.envs.wave_defense', 'WaveDefense.envs.wave_defense_no_reward', 'WaveDefense.envs.wave_defense_tabular', 'WaveDefense.envs', 'WaveDefense.envs.prefabs.bullet', 'WaveDefense.envs.prefabs.enemy_spawner', 'WaveDefense.envs.prefabs.normal_enemy', 'WaveDefense.envs.prefabs.player', 'WaveDefense.envs.prefabs'], 
     packages = find_packages(),
     include_package_data=True,
